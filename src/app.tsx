3c6c5d61bd1935e4bcc1f2fac66b2cd287568b34
Ashish <1056003401@qq.com>
2023-04-26 13:39:25 +0800

    add  react page demo

---
+++ src/app.tsx
@@ -0,0 +1,43 @@
+import { Button, ConfigProvider, theme } from 'antd';
+
+import { StyleProvider } from '@ant-design/cssinjs';
+import zhCN from 'antd/locale/zh_CN';
+
+import $styles from './app.module.css';
+
+const App = () => {
+    return (
+        <ConfigProvider
+            locale={zhCN}
+            theme={{
+                algorithm: theme.defaultAlgorithm,
+                token: {
+                    colorPrimary: '#00B96B',
+                },
+                components: {
+                    Layout: {
+                        colorBgBody: '',
+                    },
+                },
+            }}
+        >
+            <StyleProvider hashPriority="high">
+                <div className={$styles.app}>
+                    <div className={$styles.container}>
+                        欢迎来到3R教室，这是<span>React课程第一节</span>
+                        <Button
+                            type="primary"
+                            className="!tw-bg-lime-400 !tw-text-emerald-900"
+                            href="https://pincman.com/3r"
+                            target="_blank"
+                        >
+                            点此打开
+                        </Button>
+                    </div>
+                </div>
+            </StyleProvider>
+        </ConfigProvider>
+    );
+};
+
+export default App;
